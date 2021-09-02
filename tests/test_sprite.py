8f39b249acb9ad5b9e69bc657e13a327ae288772
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-09-02 20:06:48 +0900

    make sprite init test

--- tests/test_sprite.py
+++ tests/test_sprite.py
@@ -1,7 +1,6 @@
 from hopeful_stalion.gamesystem import sprite
-import pygame
 
 
 def test_sprite():
     sprite_ = sprite.Sprite(0, 0)
-    assert sprite_.image == pygame.Surface((0, 0))
+    assert sprite_.image.get_size() == (0, 0)
