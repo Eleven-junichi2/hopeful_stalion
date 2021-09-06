598f2143ddcfe51792b287cae3baf691dd48f714
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-09-06 15:01:09 +0900

    make setter for image properrty

--- hopeful_stalion/gamesystem/sprite.py
+++ hopeful_stalion/gamesystem/sprite.py
@@ -4,14 +4,21 @@ import pygame.sprite
 
 class Sprite(pygame.sprite.Sprite):
     # TODO make sprite class which can animation
-    # TODO make setter for "image" property
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.images: list[pygame.Surface] = []
         self.images.append(pygame.Surface([0, 0]))
         self.index: int = 0
-        self.image = self.images[self.index]
-        self.rect = self.image.get_rect()
+        self.__image = self.images[self.index]
+
+    @property
+    def image(self):
+        return self.__image
+
+    @image.setter
+    def image(self, value: pygame.Surface):
+        self.__image = value
+        self.image.get_rect()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
