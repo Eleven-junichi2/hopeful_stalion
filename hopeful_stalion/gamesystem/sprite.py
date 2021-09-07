6dceb4ab66eef62274d4a46b23ea094a441cb856
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-09-07 18:14:50 +0900

    make Sprite class docstring

--- hopeful_stalion/gamesystem/sprite.py
+++ hopeful_stalion/gamesystem/sprite.py
@@ -1,16 +1,26 @@
+from abc import ABCMeta
+
 import pygame
 import pygame.sprite
 
 
-class Sprite(pygame.sprite.Sprite):
-    # TODO make sprite class which can animation
+class Sprite(pygame.sprite.Sprite, metaclass=ABCMeta):
+    """
+    'images' list object need to have some Surface object as item because
+    'image' attribute is set from item of 'images' indexed by
+    anim_index.
+
+    Attributes:
+        images list[pygame.Surface]:
+        anim_index int:
+        image pygame.Surface: draw method blit a surface with this attribute.
+    """
+
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.images: list[pygame.Surface] = []
-        self.images.append(pygame.Surface([0, 0]))
-        self.index: int = 0
-        self.__image = self.images[self.index]
-        self.rect = self.image.get_rect()
+        self.__anim_index: int = 0
 
     @property
     def image(self):
@@ -20,6 +30,15 @@ class Sprite(pygame.sprite.Sprite):
     def image(self, value: pygame.Surface):
         self.__image = value
         self.__image.get_rect()
+
+    @property
+    def anim_index(self):
+        return self.__anim_index
+
+    @anim_index.setter
+    def anim_index(self, value):
+        self.__anim_index = value
+        self.image = self.images[self.anim_index]
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
