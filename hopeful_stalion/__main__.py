f41054cd05b110bf0f681016966b38e3bd431915
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2021-09-11 16:11:46 +0900

    move codes about ui to ui.py module from __main.py

--- hopeful_stalion/__main__.py
+++ hopeful_stalion/__main__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from gamesystem import scene_trans
 from gamesystem import sprite
+from gamesystem import ui
 import sys
 import json
 
@@ -112,121 +113,7 @@ class GameSceneManager(scene_trans.SceneManager):
 # TODO: make config scene to workable
 # TODO: make screen camera
 # TODO: make widget clickable
-
-
-class Widget:
-    """This class use to make UI"""
-
-    def __init__(self, pos: list = [0, 0], size: list = [0, 0],
-                 id: str = "") -> None:
-        self.pos = pos
-        self.size = size
-        self.id = id
-
-    def render(self):
-        pass
-
-
-class TextWidget(Widget):
-    def __init__(self, font: pygame.font.Font, text="",
-                 *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.font = font
-        self.__text = text
-        self.__update_size_by_text(text)
-
-    @property
-    def text(self):
-        return self.__text
-
-    @text.setter
-    def text(self, text):
-        self.__update_size_by_text(text)
-        self.__text = text
-
-    def __update_size_by_text(self, text):
-        self.size = self.font.size(text)
-
-    def render(self, *args, **kwargs) -> pygame.Surface:
-        return self.font.render(self.text, *args, **kwargs)
-
-
-class UILayout(Widget):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.layout: list[Widget] = []
-
-
-class UIBoxLayout(UILayout):
-    """This class makes it easier to lay out Widgets"""
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.orientaion = "vertical"
-        self.spacing = 0
-
-    def add_widget(self, widget: Widget):
-        layout_size = len(self.layout)
-        layout_max_index = layout_size - 1
-        if self.orientaion == "vertical":
-            if layout_size == 0:
-                widget.pos = [self.pos[0],
-                              self.pos[1]]
-            elif layout_size > 0:
-                last_widget_pos = self.layout[layout_max_index].pos
-                last_widget_size = self.layout[layout_max_index].size
-                widget.pos = [self.pos[0],
-                              last_widget_pos[1] +
-                              last_widget_size[1] +
-                              self.spacing]
-            self.layout.append(widget)
-        elif self.orientaion == "horizontal":
-            # TODO make layout when horizontal
-            pass
-
-
-class UIGameMenu(UIBoxLayout):
-    def __init__(self, default_menu_choice: int = 0,
-                 *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.__choice_num = default_menu_choice
-
-    @property
-    def choice_num(self):
-        return self.__choice_num
-
-    @choice_num.setter
-    def choice_num(self, value):
-        if value < self.__choice_num:  # __iadd__
-            if self.__choice_num > 0:
-                self.__choice_num = value
-            else:
-                self.__choice_num = self.menu_item_num() - 1
-        elif value > self.__choice_num:  # __isub__
-            if self.__choice_num < self.menu_item_num() - 1:
-                self.__choice_num = value
-            else:
-                self.__choice_num = 0
-
-    def current_choice(self) -> Widget:
-        return self.layout[self.choice_num]
-
-    def menu_item_num(self) -> int:
-        return len(self.layout)
-
-    def menu_cursor_pos(self, cursor_size: list, anchor: str = "left"):
-        """
-         Args:
-            anchor (str): 'left' or 'right'
-        """
-        if anchor == "left":
-            cursor_pos_x = self.current_choice().pos[0] - cursor_size[0]
-            cursor_pos_y = self.current_choice().pos[1]
-        elif anchor == "right":
-            cursor_pos_x = self.current_choice(
-            ).pos[0] + self.current_choice().size[0]
-            cursor_pos_y = self.current_choice().pos[1]
-        return cursor_pos_x, cursor_pos_y
+# TODO: make horse sprite image
 
 
 class HorseSprite(sprite.Sprite):
@@ -246,13 +133,14 @@ class SelectGameDataScene(scene_trans.Scene):
     # TODO make scene: select and start savedata
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.menu_ui = UIGameMenu(pos=[SCRN_WIDTH / 2.75, SCRN_HEIGHT / 3.5])
+        self.menu_ui = ui.UIGameMenu(
+            pos=[SCRN_WIDTH / 2.75, SCRN_HEIGHT / 3.5])
         # self.sm.game.databank
-        # self.menu_ui.add_widget(TextWidget(
+        # self.menu_ui.add_widget(ui.TextWidget(
         #     self.font_menu, text=self.sm.game.gametext.get_text(
         #         "cancel_c"),
         #     id="cancel"))
-        # self.menu_ui.add_widget(TextWidget(
+        # self.menu_ui.add_widget(ui.TextWidget(
         #     self.font_menu, text=self.sm.game.gametext.get_text(
         #         "confirm_config"),
         #     id="confirm"))
@@ -293,12 +181,13 @@ class ConfigScene(scene_trans.Scene):
         self.font_menu = pygame.font.Font(
             str(font_dir / "misaki_gothic.ttf"), 24)
 
-        self.menu_ui = UIGameMenu(pos=[SCRN_WIDTH / 2.75, SCRN_HEIGHT / 3.5])
-        self.menu_ui.add_widget(TextWidget(
+        self.menu_ui = ui.UIGameMenu(
+            pos=[SCRN_WIDTH / 2.75, SCRN_HEIGHT / 3.5])
+        self.menu_ui.add_widget(ui.TextWidget(
             self.font_menu, text=self.sm.game.gametext.get_text(
                 "cancel_config"),
             id="cancel"))
-        self.menu_ui.add_widget(TextWidget(
+        self.menu_ui.add_widget(ui.TextWidget(
             self.font_menu, text=self.sm.game.gametext.get_text(
                 "confirm_config"),
             id="confirm"))
@@ -357,20 +246,20 @@ class TitleScene(scene_trans.Scene):
         self.font_titlemenu = pygame.font.Font(
             str(font_dir / "misaki_gothic.ttf"), 24)
 
-        self.menu_ui = UIGameMenu(pos=[SCRN_WIDTH / 3, SCRN_HEIGHT / 2.9])
+        self.menu_ui = ui.UIGameMenu(pos=[SCRN_WIDTH / 3, SCRN_HEIGHT / 2.9])
         self.menu_ui.spacing = 16
         self.menu_ui.add_widget(
-            TextWidget(self.font_titlemenu,
-                       self.sm.game.gametext.get_text("title_game_start"),
-                       id="start"))
+            ui.TextWidget(self.font_titlemenu,
+                          self.sm.game.gametext.get_text("title_game_start"),
+                          id="start"))
         self.menu_ui.add_widget(
-            TextWidget(self.font_titlemenu,
-                       self.sm.game.gametext.get_text("title_config"),
-                       id="config"))
+            ui.TextWidget(self.font_titlemenu,
+                          self.sm.game.gametext.get_text("title_config"),
+                          id="config"))
         self.menu_ui.add_widget(
-            TextWidget(self.font_titlemenu,
-                       self.sm.game.gametext.get_text("title_exit"),
-                       id="exit"))
+            ui.TextWidget(self.font_titlemenu,
+                          self.sm.game.gametext.get_text("title_exit"),
+                          id="exit"))
 
         self.font_menu_cursor = pygame.font.Font(
             str(font_dir / "misaki_gothic.ttf"), 24)
